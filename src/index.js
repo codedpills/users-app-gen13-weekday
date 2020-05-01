6efc806e0d254e78f555ec2bd5a656862dc37667
zak <codedpills@gmail.com>
2020-05-01 23:10:29 +0000

    chore: add firebase configs

--- src/index.js
+++ src/index.js
@@ -3,12 +3,27 @@ import ReactDOM from "react-dom";
 import Router from "./Router";
 import "./index.css";
 import * as serviceWorker from "./serviceWorker";
-import { createStore, applyMiddleware } from "redux";
+import { compose, createStore, applyMiddleware } from "redux";
 import { Provider } from "react-redux";
-import thunk  from "redux-thunk";
-import usersReducer from "./store/usersReducer";
+import thunk from "redux-thunk";
+import { reactReduxFirebase, getFirebase } from "react-redux-firebase";
+import { reduxFirestore, getFirestore } from "redux-firestore";
+import firebase from "./Firebase/Firebase";
+import rootReducer from "./store/rootReducer";
 
-const store = createStore(usersReducer, applyMiddleware(thunk));
+const rrfConfig = {
+  userProfile: "users",
+  useFirestoreForProfile: true, // Firestore for Profile instead of Realtime DB
+};
+
+const store = createStore(
+  rootReducer,
+  compose(
+    reactReduxFirebase(firebase, rrfConfig),
+    reduxFirestore(firebase),
+    applyMiddleware(thunk.withExtraArgument({ getFirebase, getFirestore }))
+  )
+);
 
 ReactDOM.render(
   <Provider store={store}>
