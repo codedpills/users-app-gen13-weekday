b3ece85760a819e8a113940b1be1adf7ca75f851
zak <codedpills@gmail.com>
2020-05-09 00:57:49 +0000

    chore: add cases for error, pending, success

--- src/store/usersActions.js
+++ src/store/usersActions.js
@@ -1,22 +1,35 @@
+import * as actionTypes from "./actionTypes";
+
 export const addUser = (user) => {
-    return {
-        type: 'ADD_USER',
-        payload: user
-    }
-}
+  console.log("addUser function called!");
+  return (dispatch, getState, { getFirestore }) => {
+    let firestore = getFirestore();
+    dispatch({ type: actionTypes.ACTION_PENDING });
+    firestore
+      .collection("users")
+      .add(user)
+      .then((res) => {
+        console.log(res);
+        dispatch({ type: actionTypes.ADD_USER_SUCCESS });
+      })
+      .catch((error) => {
+        console.log(error);
+        dispatch({ type: actionTypes.ADD_USER_ERROR, payload: error });
+      });
+  };
+};
 
 export const deleteUser = (user_id) => {
-    return {
-        type: 'DELETE_USER',
-        payload: user_id
-    }
-}
+  return {
+    type: "DELETE_USER",
+    payload: user_id,
+  };
+};
 
 export const editUser = (user_id, updated_info) => {
-    return {
-        type: 'EDIT_USER',
-        user_id: user_id,
-        updated_info: updated_info
-    }
-}
-
+  return {
+    type: "EDIT_USER",
+    user_id: user_id,
+    updated_info: updated_info,
+  };
+};
