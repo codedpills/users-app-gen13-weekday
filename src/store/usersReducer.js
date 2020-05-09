9d9c4cd1acd5c24f49bd102aaf24ad39de5b23cc
zak <codedpills@gmail.com>
2020-05-09 01:00:04 +0000

    chore: add state for pending, success, error

--- src/store/usersReducer.js
+++ src/store/usersReducer.js
@@ -1,56 +1,51 @@
 import { v4 as uuid } from 'uuid';
+import * as actionTypes from './actionTypes';
 
 const initialState = {
-  users: [
-    {
-      id: "1",
-      name: "Ahmed Zaky",
-      email: "ahmed@email.com",
-      gen: 1
-    },
-    {
-      id: "2",
-      name: "Emmanuel Adebayor",
-      email: "emmanuel@email.com",
-      gen: 4
-    },
-    {
-      id: "3",
-      name: "Asamoah Gyan",
-      email: "asamoah@email.com",
-      gen: 7
-    },
-    {
-      id: "4",
-      name: "Stephen Appiah",
-      email: "stephen@email.com",
-      gen: 7
-    }
-  ]
+  pending: false,
+  success: false,
+  error: null
 };
 
 const usersReducer = (state = initialState, action) => {
   switch (action.type) {
-    case "ADD_USER":
-      const newUser = {
-        id: uuid(),
-        name: action.payload.name,
-        email: action.payload.email,
-        gen: action.payload.gen
-      };
-      return { ...state, users: [...state.users, newUser] };
-    case "DELETE_USER":
-      const filteredUsers = state.users.filter(user => user.id !== action.payload);
-      return {...state, users: filteredUsers}
-    case "EDIT_USER":
-      const editedUsers = state.users.map(user => {
-        if (user.id === action.user_id) {
-          return {...user, ...action.updated_info}
-        } else {
-          return user;
-        }
-      });
-      return {...state, users: editedUsers}
+    case actionTypes.ACTION_PENDING:
+      console.log('pending called!')
+      return {...state, pending: true}
+    case actionTypes.ADD_USER_SUCCESS:
+      return {...state, pending: false, success: true}
+    case actionTypes.ADD_USER_ERROR:
+      return {...state, pending: false, error: action.payload} 
+    case actionTypes.EDIT_USER_SUCCESS:
+      return {...state, pending: false, success: true}
+    case actionTypes.EDIT_USER_ERROR:
+      return {...state, pending: false, error: action.payload} 
+    case actionTypes.DELETE_USER_SUCCESS:
+      return {...state, pending: false, success: true}
+    case actionTypes.DELETE_USER_ERROR:
+      return {...state, pending: false, error: action.payload} 
+
+
+    // case "ADD_USER":
+    //   const newUser = {
+    //     id: uuid(),
+    //     name: action.payload.name,
+    //     email: action.payload.email,
+    //     gen: action.payload.gen
+    //   };
+    //   return { ...state, users: [...state.users, action.payload] };
+    // case "DELETE_USER":
+    //   const filteredUsers = state.users.filter(user => user.id !== action.payload);
+    //   return {...state, users: filteredUsers}
+    // case "EDIT_USER":
+    //   const editedUsers = state.users.map(user => {
+    //     if (user.id === action.user_id) {
+    //       return {...user, ...action.updated_info}
+    //     } else {
+    //       return user;
+    //     }
+    //   });
+    //   return {...state, users: editedUsers}
     default:
       return state;
   }
