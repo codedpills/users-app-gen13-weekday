c0413c4ad2d4c531117214c420431b1397a0fbb0
zak <codedpills@gmail.com>
2020-05-15 19:36:45 +0000

    chore: wire up sign up using firebase

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,31 +1,58 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import { connect } from 'react-redux';
+import { signUpWithEmail } from '../store/authActions';
 
 class Signup extends Component {
   constructor(props) {
-      super(props);
+    super(props);
     this.state = {
       username: "",
       email: "",
       password: "",
     };
   }
+  handleChange = (e) => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+  handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(this.state);
+    this.props.signUpWithEmail(this.state.email, this.state.password)
+    this.setState({ username: "", email: "", password: "" });
+  };
   render() {
+    const { username, email, password } = this.state;
     return (
       <>
         <h3>Sign up Here</h3>
-        <form>
+        <form onSubmit={this.handleSubmit}>
           <div className="form-input">
             <label htmlFor="username">Username</label>
-            <input type="text" name="username" />
+            <input
+              type="text"
+              name="username"
+              value={username}
+              onChange={this.handleChange}
+            />
           </div>
           <div className="form-input">
             <label htmlFor="email">Email</label>
-            <input type="email" name="email" />
+            <input
+              type="email"
+              name="email"
+              value={email}
+              onChange={this.handleChange}
+            />
           </div>
           <div className="form-input">
             <label htmlFor="password">Password</label>
-            <input type="password" name="password" />
+            <input
+              type="password"
+              name="password"
+              value={password}
+              onChange={this.handleChange}
+            />
           </div>
           <div className="input">
             <button type="submit">Sign up</button>
@@ -37,4 +64,8 @@ class Signup extends Component {
   }
 }
 
-export default Signup;
+const mapDispatchToProps = {
+    signUpWithEmail
+}
+
+export default connect(null, mapDispatchToProps)(Signup);
