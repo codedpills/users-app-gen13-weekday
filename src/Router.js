1401addced7f1b61b9f455c0586bc05a65b986c7
zak <codedpills@gmail.com>
2020-05-15 15:10:51 +0000

    chore: add routes for login and signup

--- src/Router.js
+++ src/Router.js
@@ -2,11 +2,15 @@ import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 import App from './App';
 import EditForm from './components/EditForm';
+import Signup from './components/Signup';
+import Login from './components/Login';
 
 const Router = () => {
     return (
         <BrowserRouter>
             <Route exact path="/" component={App} />
+            <Route path="/signup" component={Signup} />
+            <Route path="/login" component={Login} />
             <Route path="/edit/:id" component={EditForm} />
         </BrowserRouter>
     );
